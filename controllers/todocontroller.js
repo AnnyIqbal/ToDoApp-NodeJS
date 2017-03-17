c9d1fe4c2a097aee842afae607ba618fa5aaca6a
Anny <quratulann_iqbal@hotmail.com>
2017-03-17 14:43:33 +0500

    it now adds n dlts data on the list

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,17 +1,27 @@
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var data = [{item: 'hello world'}, {item: 'good going'}, {item: 'keep it up'}];
+
 module.exports = function(app) {
     // get data
     app.get('/', function(req,res){
         res.render('home');
     })
     app.get('/todo', function(req,res) {
-        res.render('todo');
+        res.render('todo', {'todos': data});
     });
     // post data (add a todo)
-    app.post('/todo', function(req,res) {
-        
+    app.post('/todo', urlencodedParser, function(req,res) {
+        data.push(req.body);
+        res.json(data);
     });
     // delete data (dlt a todo)
-    app.delete('/todo', function(req,res) {
-        
+    app.delete('/todo/:item', function(req,res) {
+        data = data.filter(function(todo) {
+            return todo.item.replace(/ /g, "-") !== req.params.item;
+        });
+        res.json(data);
     });
 };
